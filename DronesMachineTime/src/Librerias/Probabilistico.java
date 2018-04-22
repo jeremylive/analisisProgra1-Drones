9a0bb1d9b7171b3cfcb03a8bca2f70b650953380
Juan Guevara <juanmagueji@gmail.com>
2018-04-22 14:07:20 -0600

    Update Probabilistico.java
    Idea Inicial del Algoritmo Probabilista /*working*/

--- DronesMachineTime/src/Librerias/Probabilistico.java
+++ DronesMachineTime/src/Librerias/Probabilistico.java
@@ -1,6 +1,41 @@
 package Librerias;
 
+import Estructura.Grafo;
+import Estructura.NodoGrafo;
+import java.util.List;
+import java.util.Random;
+
 public class Probabilistico 
 {
-    
-}
+    /**
+     * Constructor.
+     * 
+     */
+    public Probabilistico(int cantViajes, Grafo grafo)
+    {
+        int cantEstaciones = grafo.getNodos().size();
+        
+        for(int i=0; i<cantEstaciones; i++)
+        {
+        
+            Random aleatorio = new Random(System.currentTimeMillis());
+
+            int estacionAleatoria = aleatorio.nextInt(cantEstaciones);
+
+            List<NodoGrafo> Nodos = grafo.getNodos();
+
+            NodoGrafo estacionElegida = Nodos.get(estacionAleatoria);
+
+            establecerHorario(estacionElegida, cantViajes);
+
+        }
+        
+        
+        
+               
+    };
+    void establecerHorario(NodoGrafo estacionElegida, int cantViajes)
+    {
+        
+    }
+    };
